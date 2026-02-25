8f04a6d32ecc9c8b93474c3ded87acc50e341d4c
paulo fer <paulofervboas@gmail.com>
2026-02-25 20:39:33 -0300

    adicionando swagger ui para documentaçao da api

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import tarefaRouter from './routes/tarefasRoutes.js';
 
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from "module"
+const require = createRequire(import.meta.url);
+const outputJson = require("./outputSwagger.json")
+
 const server = express();
 
 server.use(express.json());//ler dados json serrializados 
 
-server.use("/tarefa", tarefaRouter);
+//rota da view da swagger
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(outputJson));
 
+server.use("/tarefa", tarefaRouter);
 
 server.listen(5000, function () {
     console.log("servidor web funcionando");
