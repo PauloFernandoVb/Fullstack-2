15ecb69a0595b771644f75c84f69e27086be0817
paulo fer <paulofervboas@gmail.com>
2026-02-25 22:05:27 -0300

    criando a crud de usuarios

---
+++ controllers/usuarioController.js
@@ -0,0 +1,106 @@
+
+const usuarios = [
+    {
+        id: Date.now(),
+        nome: "Paulo",
+        email: "paulofervboas@gmail.com",
+        nivelAcesso: "A",
+        urlFotoPerfil: "/https://12331",
+        celular: "999999999"
+    }
+];
+
+export default class usuarioController {
+
+    cadastrar(req, res) {
+        try {
+            let nome = req.body.nome;
+            let email = req.body.email;
+            let nivelAcesso = req.body.nivelAcesso;
+            let urlFotoPerfil = req.body.urlFotoPerfil;
+            let celular = req.body.celular;
+
+            if (!nome || !email || !nivelAcesso || !urlFotoPerfil || !celular) {
+                return res.status(400).json({ msg: "Parametros incorretos!, todos os dados sao necessarios!" })
+            } else {
+                usuarios.push({
+                    id: Date.now(),
+                    nome, email, nivelAcesso, urlFotoPerfil, celular
+                });
+                return res.status(201).json({ msg: "Usuario criado com sucesso!" });
+            }
+        }
+        catch (ex) {
+            console.error(ex.message);
+            return res.status(500).json({ msg: "erro ao processar requisiçao" });
+        }
+    }
+    atualizar(req, res) {
+        try {
+            let { nome, email, nivelAcesso, urlFotoPerfil, celular } = req.body;
+
+            if (!nome || !email || !nivelAcesso || !urlFotoPerfil || !celular) {
+                return res.status(400).json({ msg: "Parametros incorretos!, todos os dados sao necessarios!" });
+            } else {
+                let UsuarioEncontrado = usuarios.filter(t => t.id == id);
+                if (UsuarioEncontrado.length == 0) {
+                    return res.status(404).json({ msg: "Usuario nao encontrado!" })
+                } else {
+                    UsuarioEncontrado[0].nome = nome;
+                    UsuarioEncontrado[0].email = email;
+                    UsuarioEncontrado[0].nivelAcesso = nivelAcesso;
+                    UsuarioEncontrado[0].urlFotoPerfil = urlFotoPerfil;
+                    UsuarioEncontrado[0].celular = celular;
+                    res.status(200).json({ msg: "Usuario Atualizado" });
+                }
+            }
+        }
+        catch (ex) {
+            console.error(ex.message);
+            return res.status(500).json({ msg: "erro ao processar requisiçao" });
+        }
+    }
+    listar(req, res) {
+        try {
+            return res.status(200).json(usuarios);
+        }
+        catch (ex) {
+            console.error(ex.message);
+            return res.status(500).json({ msg: "erro ao processar requisiçao" });
+        }
+    }
+    obter(req, res) {
+        try {
+            let id = req.params.id;
+            let UsuarioEncontrado = usuarios.filter(t => t.id == id);
+
+            if (UsuarioEncontrado.length > 0) {
+                return res.status(200).json(UsuarioEncontrado[0]);
+            }
+            return res.status(404).json({ msg: 'nenhum Usuario encontrada!' });
+        }
+        catch (ex) {
+            console.error(ex.message);
+            return res.status(500).json({ msg: "erro ao processar requisiçao" });
+        }
+    }
+    deletar(req, res) {
+        try {
+            let id = req.params.id;
+            let UsuarioEncontrado = usuarios.filter(t => t.id == id);
+            if (UsuarioEncontrado.length == 0) {
+                return res.status(404).json({ msg: "Usuario nao encontrado!" });
+
+            } else {
+                usuarios = usuarios.filter(t => t.id != UsuarioEncontrado[0].id);
+               
+                return res.status(200).json({ msg: "Usuario deletada" });
+            }
+        }
+
+        catch (ex) {
+            console.error(ex.message);
+            return res.status(500).json({ msg: "erro ao processar requisiçao" });
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import tarefaRouter from './routes/tarefasRoutes.js';
+import usuarioRouter from './routes/usuarioRoutes.js'
 
 import swaggerUi from 'swagger-ui-express';
 import { createRequire } from "module"
@@ -14,6 +15,7 @@ server.use(express.json());//ler dados json serrializados
 server.use("/docs", swaggerUi.serve, swaggerUi.setup(outputJson));
 
 server.use("/tarefa", tarefaRouter);
+server.use("/usuario",usuarioRouter);
 
 server.listen(5000, function () {
     console.log("servidor web funcionando");
